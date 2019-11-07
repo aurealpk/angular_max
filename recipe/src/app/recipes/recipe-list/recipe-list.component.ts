995a31ae7719ff7be9af434e63f6a5baee57727f
Paweł <au.pawel@gmail.com>
2019-11-07 21:58:34 +0100

    Create a structure of recipe-list and add data to view

--- recipe/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipies: [];
+  recipies: Recipe [] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://thewoksoflife.com/wp-content/uploads/2019/08/fried-brown-rice-12.jpg')
+  ];
 
   constructor() { }
 
