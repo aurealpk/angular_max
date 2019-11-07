b255f461fdde1d0764c6ba211028b1a0c0becc9d
Paweł <au.pawel@gmail.com>
2019-11-07 20:30:24 +0100

    Poprawki po błędnym wpisie recipies

--- recipe/src/app/app.module.ts
+++ recipe/src/app/app.module.ts
@@ -4,12 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipiesComponent } from './recipies/recipies.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { ItemComponent } from './recipes/recipe-list/recipe/item/item.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
@@ -17,12 +14,9 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipiesComponent,
     RecipesComponent,
     RecipeListComponent,
-    RecipeDetailsComponent,
     RecipeDetailComponent,
-    ItemComponent,
     RecipeItemComponent,
     ShoppingListComponent
   ],
